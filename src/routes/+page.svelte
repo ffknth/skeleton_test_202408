5295fded8f87735b477e41a0eb7de35d9d3df22d
unknown <IGPF-2400013@IGPF-2400013.ignitionpoint.local>
2024-08-01 17:58:51 +0900

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<div class="container mx-auto p-8 space-y-8">
+	<h1 class="h1">Hello Skeleton</h1>
+	<div class="container five columns">
+		<img src="src/image/yz25.jpg" class="u-max-full-width">
+	</div>
+	<p>Hello World!! THis is a sample page!</p>
+	<section>
+		<a class="btn variant-filled-primary" href="https://kit.svelte.dev/">SvelteKit</a>
+		<a class="btn variant-filled-secondary" href="https://tailwindcss.com/">Tailwind</a>
+		<a class="btn variant-filled-tertiary" href="https://github.com/">GitHub</a>
+	</section>
+	<button type="button" class="btn-icon variant-filled">(icon)</button>
+</div>
